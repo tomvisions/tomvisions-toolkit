be449d9a695846efc387b6fa2075486275b2bf1e
Tom <tomc@tomvisions.com>
2024-02-13 11:51:47 -0500

    updating upload

--- src/commands/system/upload.ts
+++ src/commands/system/upload.ts
@@ -2,30 +2,42 @@ import {Command} from 'commander';
 const axios = require('axios');
 import imageToUri from 'image-to-uri';
 import {system} from "../common";
+import { SocketAddress } from 'net';
 
 interface Options {
     directory: string,
     gallery: string
+    file: string
 }
 
 export class Upload {
     private async Run(options: Options) {
 
-        const acceptableExtensions = ['.jpeg', '.jpg', '.JPG', '.PNG', '.png']
+       // const acceptableExtensions = ['.jpeg', '.jpg', '.JPG', '.PNG', '.png']
         try {
+            const file = await system.readFilefromPath(options.file)
 
-            const files = await system.readDirectory(options.directory);
+            for(let row of file.split("\n")){
+                const rowItems = row.split(",");    
+                console.log(rowItems);
+
+//                parsedData.push(rowItems[0].toString() + rowItems[1].toString());
+              }
+
+           /* const files = await system.readDirectory(options.directory);
             for (let file of files) {
                 for (let extension of acceptableExtensions) {
                     if (file.includes(extension)) {
                         const theFile = imageToUri(`${options.directory}/${file}`);
-                        await axios.postForm('http://127.0.0.1:3000/api/v1/gallery/image', {
-                            gallery_id: options.gallery,
-                            file: theFile
+                        await axios.postForm('http://127.0.0.1:3500/user', {
+                            name: options.gallery,
+                            email: theFile
+                            phone: SocketAddress,
+                            description: 
                         })
                     }
                 }
-            }
+            }  */
 
         } catch (error) {
             console.log('the error');
@@ -37,9 +49,8 @@ export class Upload {
         const program = new Command();
         program
             .name('upload')
-            .description('Renames files found to Compares currently configured employees in Dynamo against the corporate active directory to determine if any users are inactive in the corporate AD')
-            .option('-d, --directory <value>', 'The folder were the images are')
-            .option('-g, --gallery <value>', 'The gallery id to import for')
+            .description('Reads the csv file and posts data through API.')
+            .option('-f, --file <value>', 'The gallery id to import for')
             .action(this.Run);
         return program;
     }
